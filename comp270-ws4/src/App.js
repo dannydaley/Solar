3cdc525afe5e30269d46e10ba08822e5601769fe
DANNY DALEY <dd252935@falmouth.ac.uk>
2021-11-18 00:04:28 +0000

    Got 3 speheres working and rotating, will get some geometry manipulation going soon

--- comp270-ws4/src/App.js
+++ comp270-ws4/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from 'react';
+import "./App.css"
+import { Canvas, useFrame, Camera } from '@react-three/fiber';
 
-function App() {
+import { PerspectiveCamera, PositionalAudio, OrbitControls, Stars } from '@react-three/drei'
+
+function Box() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <mesh>
+        <bufferGeometry attach="geometry" color="green" />
+        <meshStandardMaterial attach="material" />
+      </mesh>
+    </>
   );
 }
 
-export default App;
+
+const SpinningMesh = ({ position, color }) => {
+  const mesh = useRef(null);
+  useFrame(()=>(mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
+  return (    
+        <mesh position={position} ref={mesh}>
+          <boxBufferGeometry attached="geometry" args={[2, 2, 2]} />
+          <meshStandardMaterial attached="material" color={color} />
+        </mesh>
+    )
+}
+
+
+
+
+const App = () => {
+    return(
+    <>
+      <Canvas colorManagement camera={{position: [-5,2, 20], fov: 20}}>
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[-10, -10, -10]} intensity={0.5} shadow-mapSize-width={1024}  shadow-mapSize-height={1024} castShadow={true} receiveShadow={true}/>
+        <pointLight position={[-10, 0, -20]} intensity={0.5} />
+        <pointLight position={[0, -10, 0]} intensity={0.5} />
+
+        <SpinningMesh position={[0, 1, 0]} color={'aqua'} castShadow="true" />
+        <SpinningMesh position={[-2, 1, -5]} color={'lightblue'} />
+        <SpinningMesh position={[5, 1, -2]} color={'red'} />
+      </Canvas>
+</>
+  )
+}
+
+export default App; 
